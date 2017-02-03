2c65f02c71e189140ba694bdd35d78082d3f0d21
maksigit <maksi123p@gmail.com>
2017-02-03 17:31:36 +0200

    Ready

---
+++ main.js
@@ -0,0 +1,196 @@
+Level_1
+
+var i;
+for(i = 0; i < 2; i++){
+    console.log(north())
+}
+
+// ========================================
+
+Level_2
+
+var i;
+for(i = 0; i < 16; i++){
+    console.log(east())
+}
+
+// =========================================
+
+Level_3
+
+var a
+for (a = 0; a < 7; a++) {
+    var b;
+    for (b = 0; b < 1; b++) {
+        console.log(south())
+    }
+    var i;
+    for (i = 0; i < 1; i++) {
+        console.log(east())
+    }
+}
+
+// =========================================
+
+Level_4
+
+var a
+for (a = 0; a < 4; a++) {
+    var b;
+    for (b = 0; b < 3; b++) {
+        console.log(south())
+    }
+    var c;
+    for (c = 0; c < 3; c++) {
+        console.log(east())
+    }
+}
+
+// =========================================
+
+Level_5
+
+var a
+for (a = 0; a < 1; a++) {
+    var b;
+    for (b = 0; b < 3; b++) {
+        console.log(east())
+    }
+    var c;
+    for (c = 0; c < 2; c++) {
+        console.log(south())
+    }
+    var d;
+    for (d = 0; d < 2; d++) {
+        console.log(west())
+    }
+    var c;
+    for (c = 0; c < 3; c++) {
+        console.log(south())
+    }
+    var d;
+    for (d = 0; d < 2; d++) {
+        console.log(west())
+    }
+    var e;
+    for (e = 0; e < 3; e++) {
+        console.log(north())
+    }
+    var d;
+    for (d = 0; d < 2; d++) {
+        console.log(west())
+    }
+    var c;
+    for (c = 0; c < 5; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 6; b++) {
+        console.log(east())
+    }
+}
+
+// =========================================
+
+Level_6
+
+var a
+for (a = 0; a < 1; a++) {
+    var c;
+    for (c = 0; c < 5; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 2; b++) {
+        console.log(east())
+    }
+    var e;
+    for (e = 0; e < 3; e++) {
+        console.log(north())
+    }
+    var b;
+    for (b = 0; b < 1; b++) {
+        console.log(east())
+    }
+    var e;
+    for (e = 0; e < 2; e++) {
+        console.log(north())
+    }
+    var b;
+    for (b = 0; b < 3; b++) {
+        console.log(east())
+    }
+    var c;
+    for (c = 0; c < 4; c++) {
+        console.log(south())
+    }
+    var d;
+    for (d = 0; d < 2; d++) {
+        console.log(west())
+    }
+    var c;
+    for (c = 0; c < 3; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 2; b++) {
+        console.log(east())
+    }
+}
+
+// =========================================
+
+Level_7
+
+var a
+for (a = 0; a < 1; a++) {
+    var c;
+    for (c = 0; c < 11; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 4; b++) {
+        console.log(east())
+    }
+    var e;
+    for (e = 0; e < 11; e++) {
+        console.log(north())
+    }
+    var b;
+    for (b = 0; b < 2; b++) {
+        console.log(east())
+    }
+    var c;
+    for (c = 0; c < 1; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 1; b++) {
+        console.log(east())
+    }
+    var c;
+    for (c = 0; c < 5; c++) {
+        console.log(south())
+    }
+    for (d = 0; d < 1; d++) {
+        console.log(west())
+    }
+    var c;
+    for (c = 0; c < 7; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 8; b++) {
+        console.log(east())
+    }
+    var c;
+    for (c = 0; c < 10; c++) {
+        console.log(south())
+    }
+    var b;
+    for (b = 0; b < 1; b++) {
+        console.log(east())
+    }
+}
+
+// =========================================
